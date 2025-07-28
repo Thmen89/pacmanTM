fd225d0f9a47a0bb9c8c29666cc93e5f5548cbf1
Thmen89 <tmenghini89@gmail.com>
2025-07-28 16:10:54 +0100

    Update and rename dev-docker-entrypoint.sh to dev-entrypoint.sh

--- dev-entrypoint.sh
+++ dev-entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-
-# Exit immediately if a command exits with a non-zero status.
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
@@ -11,5 +9,5 @@ fi
 # Install gems if they are not already installed
 bundle check || bundle install
 
-# Then exec the container's main process (what's set as 'command' in docker-compose.yml).
+# Then exec the container's main process
 exec "$@"
