0f21a8fbac4b0408397ca219511d50f1813dc201
Thmen89 <tmenghini89@gmail.com>
2025-07-28 15:28:54 +0100

    Update dev-docker-entrypoint.sh

--- dev-docker-entrypoint.sh
+++ dev-docker-entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-set -e 
+set -e
 
 echo "ENVIRONMENT: $RAILS_ENV"
 
+# Change to the application's directory
+cd $APP_PATH
+
+# Install gems if they are not already present
+bundle check || bundle install
+
 # remove pid from previous session
+rm -f tmp/pids/server.pid
 
-rm -rf $APP_PATH/tmp/pids/server.pid
-
+# Run migrations and start the server
 bundle exec rails db:migrate
 bundle exec rails webpacker:install
 bundle exec rails server -p 3000 -b 0.0.0.0
