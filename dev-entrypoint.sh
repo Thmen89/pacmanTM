aabdcc7827b57b30fbf840e59bddb4a292b68e48
Thmen89 <tmenghini89@gmail.com>
2025-07-28 16:16:19 +0100

    Update dev-entrypoint.sh

--- dev-entrypoint.sh
+++ dev-entrypoint.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -e
 
+# Change to the application's directory
+cd $APP_PATH
+
 # Remove a potentially pre-existing server.pid for Rails.
-if [ -f /var/app/tmp/pids/server.pid ]; then
-  rm /var/app/tmp/pids/server.pid
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
 fi
 
 # Install gems if they are not already installed
